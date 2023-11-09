a8dd0bc0f8d744a64da2c8a2144d6fa54fe2aca9
Kang-Minseokk <dreampilot920@naver.com>
2023-11-09 16:58:16 +0900

    config 파일 email 환경변수 전달 누락

--- config/development.py
+++ config/development.py
@@ -3,3 +3,12 @@ from config.default import *
 SQLALCHEMY_DATABASE_URI = 'mysql://root:minseok0920#@localhost/pybo_db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = "dev"
+
+# STMP 서버의 url과 port 번호
+SMTP_SERVER = 'smtp.gmail.com'
+SMTP_PORT = 465
+EMAIL_ADDR = 'm23235180@gmail.com'
+EMAIL_PASSWORD = 'bpzwmnstpwrxmevk'
+
+API_KEY = "17e8c69b251581aa18fe957963b434a4"
+CITY_NAME = "Seoul"
--- config/production.py
+++ config/production.py
@@ -3,3 +3,12 @@ from config.default import *
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'\x0c\xf63\x86\xd3E\x04\xd2\xed\x1a{T]V\xc0\x1e'
+
+# STMP 서버의 url과 port 번호
+SMTP_SERVER = 'smtp.gmail.com'
+SMTP_PORT = 465
+EMAIL_ADDR = 'm23235180@gmail.com'
+EMAIL_PASSWORD = 'bpzwmnstpwrxmevk'
+
+API_KEY = "17e8c69b251581aa18fe957963b434a4"
+CITY_NAME = "Seoul"
