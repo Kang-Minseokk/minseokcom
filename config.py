15423a74fa04ba5427b7f87a1d969e0d73e0ebc3
Kang-Minseokk <dreampilot920@naver.com>
2023-11-09 16:53:38 +0900

    config 및 alias 수정

--- config.py
+++ config.py
@@ -5,11 +5,4 @@ BASE_DIR = os.path.dirname(__file__)
 SQLALCHEMY_DATABASE_URI = 'mysql://root:minseok0920#@localhost/pybo_db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-# STMP 서버의 url과 port 번호
-SMTP_SERVER = 'smtp.gmail.com'
-SMTP_PORT = 465
-EMAIL_ADDR = 'm23235180@gmail.com'
-EMAIL_PASSWORD = 'bpzwmnstpwrxmevk'
 
-API_KEY = "17e8c69b251581aa18fe957963b434a4"
-CITY_NAME = "Seoul"
--- config/default.py
+++ config/default.py
@@ -1,3 +1,12 @@
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# STMP 서버의 url과 port 번호
+SMTP_SERVER = 'smtp.gmail.com'
+SMTP_PORT = 465
+EMAIL_ADDR = 'm23235180@gmail.com'
+EMAIL_PASSWORD = 'bpzwmnstpwrxmevk'
+
+API_KEY = "17e8c69b251581aa18fe957963b434a4"
+CITY_NAME = "Seoul"
