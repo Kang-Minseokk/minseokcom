d8ebeac26df427e9180f8447aefa420958fa77b3
Kang-Minseokk <dreampilot920@naver.com>
2023-11-10 08:20:17 +0900

    데이터베이스 경로 수정

--- config/production.py
+++ config/production.py
@@ -1,6 +1,6 @@
 from config.default import *
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+SQLALCHEMY_DATABASE_URI = 'mysql://root:minseok0920#@localhost/pybo_db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'\x0c\xf63\x86\xd3E\x04\xd2\xed\x1a{T]V\xc0\x1e'
 
