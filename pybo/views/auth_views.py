d9d3b2e9618ce541f6e8dcb5b48a5060087b0a2e
Kang-Minseokk <dreampilot920@naver.com>
2023-11-22 16:58:24 +0900

    error test 13

--- pybo/views/auth_views.py
+++ pybo/views/auth_views.py
@@ -117,9 +117,9 @@ def kakao_after_login():
     if code:
         access_token = get_access_token(code)
         kakao_user_info = get_user_info(access_token)
-        name = kakao_user_info['properties']['nickname']
-        email = kakao_user_info['kakao_account']['email']
-        profile_img = kakao_user_info['properties']['profile_image']
+        name = "kakao_user_info['properties']['nickname']"
+        email = "kakao_user_info['kakao_account']['email']"
+        profile_img = "kakao_user_info['properties']['profile_image']"
 
         # 카카오 계정이 이미 있는 경우
         already_kakao_user = User.query.filter_by(username=name).first()
