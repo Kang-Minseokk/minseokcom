48b57ed096aa02c638fe14529d17209c2d591c3c
Kang-Minseokk <dreampilot920@naver.com>
2023-11-22 17:18:44 +0900

    error test 16

--- pybo/views/auth_views.py
+++ pybo/views/auth_views.py
@@ -117,7 +117,7 @@ def kakao_after_login():
     if code:
         access_token = get_access_token(code)
         kakao_user_info = get_user_info(access_token)
-        print(kakao_user_info)
+        print(type(kakao_user_info))
         name = 'kakao_user_info["properties"]["nickname"]'
         email = "kakao_user_info['kakao_account']['email']"
         profile_img = "kakao_user_info['properties']['profile_image']"
