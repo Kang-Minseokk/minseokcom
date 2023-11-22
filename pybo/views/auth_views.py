7fb43b90f329d87c2460aeda93c58ac1823b1a17
Kang-Minseokk <dreampilot920@naver.com>
2023-11-22 16:04:30 +0900

    modify

--- pybo/views/auth_views.py
+++ pybo/views/auth_views.py
@@ -116,16 +116,14 @@ def kakao_after_login():
 
     # 'code' 매개변수의 값 가져오기
     code = query_params.get('code')[0] if 'code' in query_params else None
-    print(code)
 
     # 카카오 인증을 성공한 경우
     if code:
         access_token = get_access_token(code)
         kakao_user_info = get_user_info(access_token)
-        print(kakao_user_info)
-
+        name = kakao_user_info['properties']['nickname']
         email = kakao_user_info['kakao_account']['email']
-
+        profile_img = kakao_user_info['properties']['profile_image']
 
         # 카카오 계정이 이미 있는 경우
         already_kakao_user = User.query.filter_by(username=name).first()
