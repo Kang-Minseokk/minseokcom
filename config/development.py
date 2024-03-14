2226dc0783e56476e293c98465acf9000f58f2e1
Kang-Minseokk <dreampilot920@naver.com>
2024-03-14 21:48:33 +0900

    mysql user grant ip

--- config/development.py
+++ config/development.py
@@ -1,6 +1,6 @@
 from config.default import *
 
-SQLALCHEMY_DATABASE_URI = 'mysql://root:Minseok0920#@3.34.12.179/pybo_db'
+SQLALCHEMY_DATABASE_URI = 'mysql://user1:Minseok0920#@3.34.12.179/pybo_db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = "dev"
 
